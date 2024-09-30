b63ca4c101434cc359b6da9cc06cacd14a71ab0c
Seong57 <gustjr7297@naver.com>
2024-09-30 14:14:34 +0900

    init project

---
+++ src/main/java/sch/soonjomannam/soonjmannamfull/SoonjmannamFullApplication.java
@@ -0,0 +1,13 @@
+package sch.soonjomannam.soonjmannamfull;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SoonjmannamFullApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SoonjmannamFullApplication.class, args);
+    }
+
+}
